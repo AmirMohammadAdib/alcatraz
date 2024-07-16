2417f13193f30fc0f73b20db7f6c93af372ec3e1
amir <amiradib483@gmail.com>
2024-07-16 15:58:59 +0330

    CREATE_ADMIN_CONTROLLER: write comments for auth (admin) routes

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,19 @@ use App\Http\Controllers\Admin\Auth\PlayerController;
 use App\Http\Controllers\Admin\Auth\RoleController;
 use Illuminate\Support\Facades\Route;
 
+// Group routes under the 'admin' prefix
 Route::group(['prefix' => 'admin'], function(){
+    
+    // Group routes under the 'admin/auth' prefix
     Route::group(['prefix' => 'auth'], function(){
+        
+        // Define resource routes for 'player' which will map to PlayerController
         Route::resource('player', [PlayerController::class]);
+        
+        // Define resource routes for 'admin' which will map to AdminController
         Route::resource('admin', [AdminController::class]);
+        
+        // Define resource routes for 'role' which will map to RoleController
         Route::resource('role', [RoleController::class]);
     });
 });
