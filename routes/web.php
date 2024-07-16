74df127ee575ae423f59b10d74714a3fb24e4c7b
amir <amiradib483@gmail.com>
2024-07-16 15:54:29 +0330

    CREATE_ADMIN_CONTROLLER: write routes of auth perfix and make controllers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\Admin\Auth\AdminController;
+use App\Http\Controllers\Admin\Auth\PlayerController;
+use App\Http\Controllers\Admin\Auth\RoleController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('admin.dashboard');
+Route::group(['prefix' => 'admin'], function(){
+    Route::group(['prefix' => 'auth'], function(){
+        Route::resource('player', [PlayerController::class]);
+        Route::resource('admin', [AdminController::class]);
+        Route::resource('role', [RoleController::class]);
+    });
 });
