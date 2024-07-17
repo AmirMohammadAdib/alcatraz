af08c720516684870991064fe0fccea0cd850f17
amir <amiradib483@gmail.com>
2024-07-18 00:04:38 +0330

    create match controllers

--- database/migrations/2024_07_15_103823_create_rooms_table.php
+++ database/migrations/2024_07_15_103823_create_rooms_table.php
@@ -14,11 +14,10 @@ return new class extends Migration
         Schema::create('rooms', function (Blueprint $table) {
             $table->id();
             $table->text('link');
-            $table->decimal('fee', 10, 2)->nullable();
+            $table->decimal('fee', 10, 2)->nullable()->comment('price for login to the game');
             $table->decimal('award', 10, 2)->nullable();
-            $table->integer('winners')->comment('count of winners')->nullable();
-            $table->integer('per_killed')->nullable();
-            $table->tinyInteger('type')->default(0);
+            $table->integer('award_type')->default(0)->comment('type of award');
+            $table->tinyInteger('type')->default(0)->comment('type of game');
             $table->integer('capacity')->default(0)->comment('count of players that can be match');
             $table->integer('players')->default(0)->comment('count of current players');
             $table->tinyInteger('status')->default(0)->comment('0 => waiting to start, 1 => in process, 2 => finished, 3 => canceled');
