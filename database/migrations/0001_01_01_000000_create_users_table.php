bf769a00bf0e692eb3666db835068215cb7b6951
amir <amiradib483@gmail.com>
2024-07-21 14:57:26 +0330

    change some part of db & migrate

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,7 +19,9 @@ return new class extends Migration
             $table->tinyInteger('role')->default(0)->comment('0 => user, 1 => admin');
             $table->tinyInteger('status')->default(0);
             $table->decimal('wallet')->nullable();
+            $table->decimal('award_wallet')->nullable();
             $table->string('cart_number');
+            $table->string('shabba_number');
             $table->timestamps();
         });
     }
