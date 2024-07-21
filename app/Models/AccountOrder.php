0642abe8c775108c09cb5476404486256f4244f3
amir <amiradib483@gmail.com>
2024-07-21 15:06:05 +0330

    create all of models

---
+++ app/Models/AccountOrder.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AccountOrder extends Model
+{
+    use HasFactory;
+}
