5ccdafb74b580db698cdcd26e4e8984efac40da2
amir <amiradib483@gmail.com>
2024-07-22 15:19:35 +0330

    AdminController: create AdminRequest

---
+++ app/Http/Requests/Admin/Auth/AdminRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests\Admin\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AdminRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        if($this->isMethod('POST')){
+            return [
+                'phone' => 'required|string|max:11|min:11|unique:users,phone',
+                'username' => 'required|string|max:255||unique:users,username',
+                'status' => 'required|string',
+                'roles' => 'required|array',
+            ];
+        }else{
+            return [
+                'phone' => 'required|string|max:11|min:11|unique:users,phone,' . $this->player->id,
+                'username' => 'required|string|max:255||unique:users,username,' . $this->player->id,
+                'status' => 'required|string',
+                'roles' => 'required|array',
+            ];    
+        }
+    }
+}
