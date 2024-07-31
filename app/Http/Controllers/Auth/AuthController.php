aea8f33ebe11ae47f821a7c9881025c18c271821
amir <amiradib483@gmail.com>
2024-08-01 01:38:52 +0330

    compilite logout

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+    public function logout(){
+        Auth::logout();
+        return redirect()->route('app.index');
+    }
 }
