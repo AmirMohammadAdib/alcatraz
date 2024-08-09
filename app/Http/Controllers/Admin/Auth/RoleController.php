8fb04692eabcc1bfaabe6febdb14b469ff259d49
amir <amiradib483@gmail.com>
2024-08-09 13:44:21 +0330

    RoleController: compilited یثمثفث سثذفهخر

--- app/Http/Controllers/Admin/Auth/RoleController.php
+++ app/Http/Controllers/Admin/Auth/RoleController.php
@@ -80,8 +80,9 @@ class RoleController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Role $role)
     {
-        //
+        $role->delete();
+        return redirect()->route('role.index')->with('success', 'نقش با شناسه ' . $role->id . ' با موفقیت حذف شد');
     }
 }
